f485510f47f27c4db976c657df1dca9fa4eb61bd
oanasabau1 <oanasabau1@gmail.com>
2024-08-25 21:06:21 +0300

    began working at frontend

--- library-frontend/react-library/src/App.tsx
+++ library-frontend/react-library/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layout/navbar/Navbar'
+import { ExploreTopBooks } from './layout/homepage/ExploreTopBooks';
+import { Carousel } from './layout/homepage/Carousel';
 
 function App() {
   return (
+    <div>
     <Navbar />
+    <ExploreTopBooks />
+    <Carousel />
+    </div>
   );
 }
 
